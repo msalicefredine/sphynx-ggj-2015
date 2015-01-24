eab998dce58bf2311971a22700e0683962c64584
Mohamed Shaaban <mohamedshaaban@dhcp-128-189-75-219.ubcsecure.wireless.ubc.ca>
2015-01-24 13:53:26 -0800

    try

--- Sphynx/src/ui/GameState.java
+++ Sphynx/src/ui/GameState.java
@@ -10,9 +10,12 @@ public class GameState {
 
     private boolean atFrontPage;
     private boolean atEndPage;
-    private boolean atChallenge;
-    private boolean wonChallenge;
-    private boolean lostChallenge;
+    private boolean atHardChallenge;
+    private boolean wonHardChallenge;
+    private boolean lostHardChallenge;
+    private boolean atEasyChallenge;
+    private boolean wonEasyChallenge;
+    private boolean lostEasyChallenge;
     private boolean wonGame;
     private boolean lostGame;
     private Path path;
@@ -20,9 +23,9 @@ public class GameState {
     public GameState() {
 
         atFrontPage = false;
-        atChallenge = false;
-        wonChallenge = false;
-        lostChallenge = false;
+        atHardChallenge = false;
+        wonHardChallenge = false;
+        lostHardChallenge = false;
         atEndPage = false;
         wonGame = false;
         lostGame = false;
@@ -37,41 +40,72 @@ public class GameState {
     //================================================================================================================
     // Game State setters to be called at any change of state (basically an accumulator that is a class)
 
-    public void atFrontPage() {
+    public void setAtStart() {
         atFrontPage = true;
         atEndPage = false;
-        atChallenge = false;
+        atHardChallenge = false;
+        atEasyChallenge = false;
     }
 
-    public void atChallenge() {
-        atChallenge = true;
+    public void setAtHardChallenge() {
+        atHardChallenge = true;
+        atEasyChallenge = false;
         atFrontPage = false;
         atEndPage = false;
     }
 
-    public void wonChallenge() {
-        wonChallenge = true;
-        lostChallenge = false;
+    public void setWonHardChallenge() {
+        wonHardChallenge = true;
+        lostHardChallenge = false;
+        wonEasyChallenge = false;
+        lostEasyChallenge = false;
 
     }
 
-    public void lostChallenge() {
-        lostChallenge = true;
-        wonChallenge = false;
+    public void setLostHardChallenge() {
+        lostHardChallenge = true;
+        wonHardChallenge = false;
+        wonEasyChallenge = false;
+        lostEasyChallenge = false;
+
     }
 
-    public void atEndPage() {
+    public void setAtEasyChallenge() {
+        atHardChallenge = false;
+        atEasyChallenge = true;
+        atFrontPage = false;
+        atEndPage = false;
+    }
+
+    public void setWonEasyChallenge() {
+        wonHardChallenge = false;
+        lostHardChallenge = false;
+        wonEasyChallenge = true;
+        lostEasyChallenge = false;
+
+    }
+
+    public void setLostEasyChallenge() {
+        wonHardChallenge = false;
+        lostHardChallenge = false;
+        wonEasyChallenge = false;
+        lostEasyChallenge = true;
+
+    }
+
+    public void setAtEnd() {
         atEndPage = true;
-        atChallenge = false;
+        atHardChallenge = false;
+        atEasyChallenge = false;
         atFrontPage = false;
     }
 
-    public void wonGame() {
+    public void setWonGame() {
         wonGame = true;
         lostGame = false;
     }
 
-    public void lostGame() {
+    public void setLostGame() {
         lostGame = true;
         wonGame = false;
     }
@@ -80,34 +114,51 @@ public class GameState {
     // Game State checkers to check current state
 
     // return true if the game is on the start page
-    public boolean isAtStart() {
+    public boolean atStart() {
         return atFrontPage;
     }
 
     // return true if the game is presenting a challenge to the user
-    public boolean isAtChallenge() {
-        return atChallenge;
+    public boolean atHardChallenge() {
+        return atHardChallenge;
     }
 
     // return true if the user has won
-    public boolean isWonChallenge() {
-        return wonChallenge;
+    public boolean wonHardChallenge() {
+        return wonHardChallenge;
     }
 
     // return true if the user has lost
-    public boolean isLostChallenge() {
-        return lostChallenge;
+    public boolean lostHardChallenge() {
+        return lostHardChallenge;
     }
 
-    public boolean isAtEnd() {
+    // return true if the game is presenting a challenge to the user
+    public boolean atEasyChallenge() {
+        return atEasyChallenge;
+    }
+
+    // return true if the user has won
+    public boolean wonEasyChallenge() {
+        return wonEasyChallenge;
+    }
+
+    // return true if the user has lost
+    public boolean lostEasyChallenge() {
+        return lostEasyChallenge;
+    }
+
+    public boolean atEnd() {
         return atEndPage;
     }
 
-    public boolean isWonGame() {
+    public boolean wonGame() {
         return wonGame;
     }
 
-    public boolean isLostGame() {
+    public boolean lostGame() {
         return lostGame;
     }
+
+
 }
